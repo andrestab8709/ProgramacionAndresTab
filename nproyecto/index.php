bcb26faf331a3c21c719c69e675b04646f60523a
Unknown <andrestab57@gmail.com>
2018-09-14 14:38:50 -0500

    Php
    todo lo de php

--- nproyecto/index.php
+++ nproyecto/index.php
@@ -10,8 +10,53 @@ and open the template in the editor.
         <title>Nuevo</title>
     </head>
     <body>
+        <h1>
+            
+           
         <?php
-        // put your code here
+        //Esto es un comando
+        $Primeravariable = "2";
+          echo $Primeravariable."<br>";
+         $SegundaVariable = "7"; 
+         $Lenguaje = "javascrip";
+         echo $SegundaVariable * $Primeravariable;
+                 
+        if($Primeravariable == 2 && $SegundaVariable== 7){
+            echo 'Soy mayor';
+        }   
+        else{
+            echo "soy menor";
+        }
+        switch ($Lenguaje){
+            
+            case "php":
+            echo "php";
+               break;
+            case "javascrip":
+                echo "javascrip";
+                break;
+            case "html":
+                echo "html";
+                break;
+               
+        }
+        
+        $Arreglo = [2,"hol",6];
+        echo $Arreglo[2];
+      
+                 
+        // put your code here El Punto concatena, el mas Suma, php identifica tipo de variable
         ?>
+            
+        </h1>
+        <p>
+        <h2>
+              <?php
+              echo "esta es otra seccion";
+              
+             ?>
+        </h2>
+            
+        </p>
     </body>
 </html>
